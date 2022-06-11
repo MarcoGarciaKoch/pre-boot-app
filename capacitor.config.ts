5b13ae9a51887ef65aae8978f1781bf9c3771105
marcogarciakoch <marcogarciakoch@gmail.com>
2022-06-11 19:38:15 +0200

    ionic-project-settings-first-steps

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pre-boot-app',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
